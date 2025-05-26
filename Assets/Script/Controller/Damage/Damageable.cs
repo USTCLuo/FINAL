4f64ed6998498944386bacb1db564b32ccf0aefd
USTCLuo <luoty060401@outlook.com>
2025-05-26 21:16:19 +0800

    添加了敌人,完善了主角的逻辑

---
+++ Assets/Script/Controller/Damage/Damageable.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbced46b5b63c6cf2c0c567a4b71eb9ced34c46e38436696241a93c01bf5fd49
+size 1782
---
+++ Assets/Script/Controller/Interact/InteractTriggerListener.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c4bc63308981699b3ca5bc1a7f959775840b6617bcb63b182d32c3eb7778d080
+size 618
